ade62597fbee509db865df2c827c4fda861743a6
Kathy Chiem <kchie002@gmail.com>
2020-01-29 23:36:17 -0800

    change email message

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -20,7 +20,7 @@ module.exports = app => {
         from: 'kchie002@gmail.com',
         subject: 'Thank you for reaching out!',
         text: 'I will review your email as soon as I can!',
-        html: '<h3>Thank you for contacting me!</h3><p><strong>I will be reviewing your message as soon as I can!</strong></p><p>In the meantime if you do have any further comments or questions for me, please respond to this email and I  will get your message!</p> <p>Have a wonderful day!</p><p>Thank you,<br />Kevin Truong</p>',
+        html: '<h3>Thank you for contacting me!</h3><p><strong>I will be reviewing your message as soon as I can!</strong></p><p>In the meantime if you do have any further comments or questions for me, please respond to this email and I  will get your message!</p> <p>Have a wonderful day!</p><p>Thank you,<br />Kathy Chiem</p>',
         };
         sgMail.send(msg2);
     })
